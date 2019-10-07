0bcd550d7c59527abcab307861ae70ed942a2ecd
Ubuntu <ubuntu@ip-172-31-20-243.ec2.internal>
2019-10-07 23:48:40 +0000

    1

---
+++ month.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# Created by: Christina Ngwa
+# Created on: October 2019
+# This program checks checks what month it is
+#    with user input
+
+
+def main():
+    # This function checks checks what month it is
+
+    # input
+    print("Let's figure out what month it is.")
+    monthNum = int(input("Enter a number of a month (1-12). : "))
+
+    # process
+    if monthNum == 1:
+        # output
+        print("")
+        print("It is January.")
+        # output
+    elif monthNum == 2:
+        # output
+        print("")
+        print("It is February.")
+        # output
+    elif monthNum == 3:
+        # output
+        print("")
+        print("It is March.")
+    elif monthNum == 4:
+        # output
+        print("")
+        print("It is April.")
+    elif monthNum == 5:
+        # output
+        print("")
+        print("It is May.")
+    elif monthNum == 6:
+        # output
+        print("")
+        print("It is June.")
+        # output
+    elif monthNum == 7:
+        # output
+        print("")
+        print("It is July.")
+    elif monthNum == 8:
+        # output
+        print("")
+        print("It is August.")
+    elif monthNum == 9:
+        # output
+        print("")
+        print("It is September.")
+    elif monthNum == 10:
+        # output
+        print("")
+        print("It is October.")
+    elif monthNum == 11:
+        # output
+        print("")
+        print("It is November.")
+    elif monthNum == 12:
+        # output
+        print("")
+        print("It is Decemeber.")
+    else:
+        # output
+        print("")
+        print("Sorry, that is invaid. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
